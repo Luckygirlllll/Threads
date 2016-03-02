35d803486a0c38c77d2e57bc9226e535c13808a5
Iryna <irynakanivets@gmail.com>
2016-03-02 13:53:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/iryna/threads/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.iryna.threads;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            TextView myText =(TextView)findViewById(R.id.myText);
+            myText.setText("Nice job Iryna!");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    //method which simulates waiting
+    public void clickMyButton(View view){
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                long FutureTime=System.currentTimeMillis()+10000;
+                while(System.currentTimeMillis()  < FutureTime){
+                    synchronized (this){
+                        try{
+                            wait(FutureTime-System.currentTimeMillis());
+                        }catch (Exception e){}
+
+                    }
+                }
+                handler.sendEmptyMessage(0);
+            }
+        };
+        Thread myThread = new Thread(r);
+        myThread.start();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
